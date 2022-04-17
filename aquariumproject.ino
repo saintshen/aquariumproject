aee7e8544ca31a2024b0a932720f3b8ba3579ca0
Ming Shen <saintshen@gmail.com>
2022-04-16 20:17:08 -0400

    add screen 2

--- aquariumproject.ino
+++ aquariumproject.ino
@@ -1,5 +1,3 @@
-#include <AxisJoystick.h>
-#include <Joystick.h>
 #include <LiquidCrystal.h>
 #include <DHT.h>
 #include <OneWire.h>
@@ -10,26 +8,28 @@
 #define DHTPIN 8
 #define DHTTYPE DHT11
 #define ONE_WIRE_BUS A3
-#define SW_PIN 13
 #define VRX_PIN A0
 #define VRY_PIN A1
+#define SW_PIN 18
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-Joystick* joystick;
 
 DHT dht(DHTPIN, DHTTYPE);
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-int lcdDisplay = 1;
-
-
 int xPosition = 0;
 int yPosition = 0;
-int SW_state = 0;
-int SW_before = 0;
+int screen = 0;
+volatile byte switchClick = 0;
+volatile bool lcdDisplay = true;
+
+float humidity = 0;
+float temperature = 0;
+float waterTemperature = 0;
+float waterPH = 0;
 
 int mapX = 0;
 int mapY = 0;
@@ -44,60 +44,76 @@ float ReferenceRange = ReferenceHigh - ReferenceLow;
 
 
 void setup() {
+  pinMode(VRX_PIN, INPUT);
+  pinMode(VRY_PIN, INPUT);
+  pinMode(SW_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SW_PIN), toggleDisplay, RISING);
 
-  Serial.begin(9600);
-  dht.begin();
   lcd.begin(16, 2);
   analogWrite(LCD_BACKLIGHT_PIN, 100);
   analogWrite(LCD_CONTRAST_PIN, 50);
-
-  joystick = new AxisJoystick(SW_PIN, VRX_PIN, VRY_PIN);
-
+  dht.begin();
   sensors.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
   sensors.requestTemperatures();
 
-  float waterT = sensors.getTempCByIndex(0);
+  waterTemperature = sensors.getTempCByIndex(0);
+  
+  if (lcdDisplay) {
+    lcd.display();
+    analogWrite(LCD_BACKLIGHT_PIN, 100);
+  } else {
+    lcd.noDisplay();
+    analogWrite(LCD_BACKLIGHT_PIN, 0);
+  }
+  
 
-  SW_before = SW_state;
-  SW_state = joystick->isPress();
-  toggleDisplay();
+  xPosition = analogRead(VRX_PIN);
+  yPosition = analogRead(VRY_PIN);
 
-  lcd.setCursor(0, 0);
-  lcd.print("Humidity: " + String(h, 2));
-  lcd.setCursor(0, 1);
-  lcd.print("Temp: " + String(waterT, 2));
+  mapX = map(xPosition, 0, 1023, -512, 512);
+  mapY = map(yPosition, 0, 1023, -512, 512);
 
+  if (mapY>0){
+    screen++;
+  }
 
+  if (screen % 2) {
+    lcd.setCursor(0, 0);
+    lcd.print("Humidity: " + String(humidity, 2));
+    lcd.setCursor(0, 1);
+    lcd.print("TEMP    : " + String(temperature, 2));
+  }
+  else {
+    lcd.setCursor(0, 0);
+    lcd.print("WaterTemp:" + String(waterTemperature, 2));
+    lcd.setCursor(0, 1);
+    lcd.print("Water PH :" + String(waterPH, 2));
+  }
+  
+  
+  Serial.print("swtch clicked: " + String(switchClick));  
+  Serial.println(" | Display: " + String(lcdDisplay));
   /*
-  Serial.print("X: ");
-  Serial.print(mapX);
-  Serial.print(" | Y: ");
-  Serial.print(mapY);
-  Serial.println(" | Press: " + String(joystick->isPress()));
-  Serial.println(SW_state);
-  Serial.println(SW_before); 
+    Serial.print("X: ");
+    Serial.print(mapX);
+    Serial.print(" | Y: ");
+    Serial.print(mapY);
+    Serial.println(SW_state);
+    Serial.println(SW_before);
   */
 
 
 }
 
 void toggleDisplay() {
-  if (SW_before == 1 && SW_state == 0) {
-    if (lcdDisplay == 0) {
-      lcd.display();
-      analogWrite(LCD_BACKLIGHT_PIN, 100);
-      lcdDisplay = 1;
-    } else {
-      lcd.noDisplay();
-      analogWrite(LCD_BACKLIGHT_PIN, 0);
-      lcdDisplay = 0;
-    }
+  switchClick++;
+  if (switchClick % 2) {
+    lcdDisplay = !lcdDisplay;
   }
 }
